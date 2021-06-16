8487509ac70453effdfe7c062d6b5e10f5450537
Ko Htut <kohtut@Ko-Htuts-MacBook-Pro.local>
2021-06-16 10:49:52 +0630

    test

--- lib/constants.dart
+++ lib/constants.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 const primaryColor = Color(0xFF202020);
 const secondaryColor = Color(0xFFffffff);
 const bgColor = Color(0xFFffffff);
-const headerColor=Color(0xff00447b);
-const tableHeader=Color(0xff00447b);
+const headerColor=Colors.blue;
+const tableHeader=Colors.lightBlue;
 
 const defaultPadding = 16.0;
